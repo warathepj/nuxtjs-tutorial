8092417feed9a7ed831721919edb037ba7fd59cd
warathepj <80021220+warathepj@users.noreply.github.com>
2021-06-30 15:30:17 +0700

    1 32 21

---
+++ components/posts/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <b-carousel id="slider"
+      controls
+      indicators
+      >
+      <b-carousel-slide
+        caption="First slide"
+        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+        img-src="https://picsum.photos/1024/480/?image=52"
+      ></b-carousel-slide>
+      </b-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  
+</style>
