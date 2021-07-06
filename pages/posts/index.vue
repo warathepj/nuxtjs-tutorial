74185efd298a7704248269dc9e9acc1bead71ee6
warathepj <80021220+warathepj@users.noreply.github.com>
2021-07-06 15:10:51 +0700

    2 09 53

---
+++ pages/posts/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <b-container fluid>
+    <Header />
+    <b-container>
+      <b-row class="py-5">
+        <PostContent title="travel elephant home" content="lorem ipsum"/>
+        <PostContent />
+        <PostContent />
+      </b-row>
+    </b-container>
+  
+  </b-container>
+</template>
+
+<script>
+import Header from '@/components/posts/Header.vue'
+import PostContent from '@/components/posts/PostContent.vue'
+export default {
+  layout: 'coreLayout',
+  components: {
+    Header,
+    PostContent,
+  },
+}
+</script>
+
+<style>
+</style>
